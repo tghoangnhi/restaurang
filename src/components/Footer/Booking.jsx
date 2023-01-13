ac18e9914c88701ccbdc68aef3a7b88600fd1a95
Nhi <hnhi.trangia@gmail.com>
2023-01-14 00:54:27 +0700

    feat: connect db

--- src/components/Footer/Booking.jsx
+++ src/components/Footer/Booking.jsx
@@ -1,43 +1,87 @@
-import React from 'react';
+import { React } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set } from 'firebase/database';
 
 import SubHeading from '../SubHeading/SubHeading';
 import './Booking.css';
+// require('dotenv').config();
 
-const Booking = () => (
-  <div className='app__newsletter' id='booking'>
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESS_SENDER_ID,
+  appId: process.env.APP_ID
+};
+// console.log('apikey:' + process.env.API_KEY);
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const Booking = () => {
+  const handleSubmit = (e) => {
+    
+    e.preventDefault();
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const phone = document.getElementById('phone-number');
+    const date = document.getElementById('date');
+    const time = document.getElementById('time');
+    const number = document.getElementById('number');
+
+    const writeData = () => {
+      const db = getDatabase();
+      set(ref(db, 'data/'), {
+        name: name.value,
+        email: email.value,
+        phone: phone.value,
+        date: date.value,
+        time: time.value,
+        number: number.value
+      });
+    alert("Your booking has been made");
+    }
+    writeData();
+  }
+
+  return (
+    <div className='app__newsletter' id='booking'>
     <div className='app__newsletter-heading'>
       <SubHeading title='Booking'/>
       <h1 className='headtext__cormorant flex__center'>Book A Table</h1>
     </div>
 
-    <form action="" className='app__booking-form flex__center'>
+    <form action="" className='app__booking-form flex__center' onSubmit={handleSubmit}>
       <div className='app__booking-input'>
         <p className='p__opensans custom__title'>Name</p>
-        <input type='text' placeholder='Jane Doe' />
+        <input type='text' id='name' required/>
       </div>
       <div className='app__booking-input'>
         <p className='p__opensans custom__title'>Email</p>
-        <input type='email' placeholder='Enter your email address' />
+        <input type='email' id='email' required/>
       </div>
       <div className='app__booking-input'>
         <p className='p__opensans custom__title'>Phone Number</p>
-        <input type='number' placeholder='Enter your phone number' />
+        <input type='number' id='phone-number' required/>
       </div>
       <div className='app__booking-input'>
         <p className='p__opensans custom__title'>How many people?</p>
-        <input type='number'/>
+        <input type='number' id='number' required/>
       </div>
       <div className='app__booking-input'>
         <p className='p__opensans custom__title'>Date</p>
-        <input type='date'/>
+        <input type='date' id='date' required/>
       </div>
       <div className='app__booking-input'>
         <p className='p__opensans custom__title'>Time</p>
-        <input type='time'/>
+        <input type='time' id='time' required/>
       </div>
-      <button type='button' className='custom__button'>Book Now</button>
+      
+      <button type='submit' className='custom__button'>Book Now</button>
     </form>
   </div>
-);
+  );
+}
 
 export default Booking;
